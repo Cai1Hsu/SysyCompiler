89bd83967ca3451b3499f4cf9b1f826a98c48281
Caiyi Hsu <Cai1Hsu@outlook.com>
2025-10-04 20:57:42 +0800

    feat(analyzer): implement semantic analysis rules for OJ code analyzer
    - Add argument count and type checking for function calls
    - Add constant modification analysis
    - Register function parameters to local scope
    - Introduce helper methods for symbol creation and expression type resolution
    - Include necessary namespaces for diagnostics and immutable collections

--- SysyCompiler.Analyzer/OnlineJudgeCodeAnalyzer.cs
+++ SysyCompiler.Analyzer/OnlineJudgeCodeAnalyzer.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Collections.Immutable;
 using SysyCompiler.Analyzer.Semantic;
 using SysyCompiler.Frontend;
 using SysyCompiler.Frontend.Syntax;
@@ -14,17 +16,26 @@ public class OnlineJudgeCodeAnalyzer : SymbolAnalyzer<object>
         AnalyzeLiteralExpression += FormatStringAnalysis;
 
         // rule 'b'
-        AnalyzeFunctionDeclaration += (i, a, d) => DeclareSymbol(new FunctionSymbol(i), a, d);
-        AnalyzeVariableDefine += (i, a, d) => DeclareSymbol(new VariableSymbol(i.Item2, i.Item1), a, d);
+        AnalyzeFunctionDeclaration += (i, a, d) => DeclareSymbol(CreateFunctionSymbol(i), a, d);
+        AnalyzeVariableDefine += (i, a, d) => DeclareSymbol(CreateVariableSymbol(i.Item1, i.Item2), a, d);
 
         // rule 'c'
         AnalyzeReferenceUsage += UndefinedReferenceAnalysis;
+
+        // rule 'd'
+        AnalyzeFunctionCall += ArgumentCountAnalysis;
+
+        // rule 'e'
+        AnalyzeFunctionCall += ArgumentTypeAnalysis;
 
         // rule 'f'
         AnalyzeFunctionDeclaration += VoidFunctionReturnValueAnalysis;
 
         // rule 'g'
         AnalyzeFunctionDeclaration += ValueFunctionMissingReturnAnalysis;
+
+        // rule 'h'
+        AnalyzeReferenceUsage += ConstantModificationAnalysis;
 
         // rule 'i'
         AnalyzeVariableDeclaration += (i, a, d) => MissingSemicolonAnalysis(i.GetTokens().Last(), i.SemicolonToken, a, d);
@@ -49,6 +60,44 @@ public class OnlineJudgeCodeAnalyzer : SymbolAnalyzer<object>
 
         // rule 'm'
         AnalyzeControlFlowStatement += ControlFlowStatementNotInLoopAnalysis;
+
+        // Register function parameters to local scope
+        AnalyzeBlock += RegisterFunctionParameters;
+    }
+
+    public static VariableSymbol CreateVariableSymbol(VariableDefineSyntax define, VariableDeclarationSyntax declaration)
+    {
+        var arrayRank = define.ArrayDimensions?.Count ?? 0;
+        var type = ISymbolTypeExtensions.Parse(declaration.Type, arrayRank);
+
+        Debug.Assert(type is not null, "Type should be valid here");
+
+        return new VariableSymbol(define.Identifier, define, type, declaration.Modifier);
+    }
+
+    public static FunctionSymbol CreateFunctionSymbol(FunctionDeclarationSyntax declaration)
+    {
+        var parameterTypes = ImmutableArray.CreateBuilder<ISymbolType>();
+
+        var parameterList = declaration.ParameterList?.Items;
+        if (parameterList is not null)
+        {
+            foreach (var parameter in parameterList)
+            {
+                var arrayRank = parameter.ArrayDimensions?.Count ?? 0;
+                var parameterType = ISymbolTypeExtensions.Parse(parameter.Type, arrayRank);
+
+                Debug.Assert(parameterType is not null, "Type should be valid here");
+
+                parameterTypes.Add(parameterType);
+            }
+        }
+
+        var type = ISymbolTypeExtensions.Parse(declaration, parameterTypes.ToImmutable());
+
+        Debug.Assert(type is not null, "Type should be valid here");
+
+        return new FunctionSymbol(declaration, type);
     }
 
     public static void FormatStringAnalysis(LiteralExpressionSyntax info, ISymbolAnalyzer analyzer, SemanticDiagnosticCollector diagnostics)
@@ -232,6 +281,168 @@ public class OnlineJudgeCodeAnalyzer : SymbolAnalyzer<object>
             {
                 diagnostics.Add(new SemanticDiagnostic(SemanticErrorKind.ValueFunctionMissingReturn, info.Body.CloseBraceToken));
                 return;
+            }
+        }
+    }
+
+    public static void ArgumentCountAnalysis(FunctionCallExpressionSyntax info, ISymbolAnalyzer analyzer, SemanticDiagnosticCollector diagnostics)
+    {
+        if (info.Callee is not ReferenceExpressionSyntax directCall)
+            return;
+
+        if (analyzer.ResolveGlobal(directCall.Identifier.Text) is not FunctionSymbol functionSymbol)
+            return;
+
+        if (functionSymbol.Declaration.ParameterList?.Items.Count != info.ArgumentList.Items.Count)
+        {
+            diagnostics.Report(SemanticErrorKind.FunctionArgumentCountMismatch, info.OpenParenToken);
+        }
+    }
+
+    public static void ArgumentTypeAnalysis(FunctionCallExpressionSyntax info, ISymbolAnalyzer analyzer, SemanticDiagnosticCollector diagnostics)
+    {
+        if (info.Callee is not ReferenceExpressionSyntax directCall)
+            return;
+
+        if (analyzer.ResolveGlobal(directCall.Identifier.Text) is not FunctionSymbol functionSymbol)
+            return;
+
+        var parameters = functionSymbol.Type.ParameterTypes;
+        var arguments = info.ArgumentList.Items;
+
+        for (int i = 0; i < Math.Min(parameters.Length, arguments.Count); i++)
+        {
+            var parameter = parameters[i];
+            var argument = arguments[i].Expression;
+
+            if (ResolveExpressionType(argument, analyzer) is not ISymbolType argumentType)
+                continue;
+
+            if (!parameter.Equals(argumentType))
+            {
+                diagnostics.Add(new SemanticDiagnostic(SemanticErrorKind.FunctionArgumentTypeMismatch, directCall.Identifier));
+            }
+        }
+    }
+
+    public static ISymbolType? ResolveExpressionType(ExpressionSyntax expression, ISymbolAnalyzer analyzer)
+    {
+        switch (expression)
+        {
+            case BinaryExpressionSyntax binaryExpression:
+                var leftType = ResolveExpressionType(binaryExpression.Left, analyzer);
+                var rightType = ResolveExpressionType(binaryExpression.Right, analyzer);
+
+                if (leftType is null || !leftType.Equals(rightType))
+                    goto default; // type mismatch, resolve to null
+                else
+                    return leftType;
+
+            case LiteralExpressionSyntax literalExpression:
+                return ISymbolTypeExtensions.Parse(literalExpression);
+
+            case GroupedExpressionSyntax groupedExpression:
+                return ResolveExpressionType(groupedExpression.Expression, analyzer);
+
+            case UnaryExpressionSyntax unaryExpression:
+                return ResolveExpressionType(unaryExpression.Operand, analyzer);
+
+            case FunctionCallExpressionSyntax functionCall:
+                if (ResolveExpressionType(functionCall.Callee, analyzer) is not FunctionType functionType)
+                    goto default; // not a function
+
+                return functionType.ReturnType; // unbox the return type
+
+            case ArrayExpressionSyntax arrayAccess:
+                var baseType = ResolveExpressionType(arrayAccess.Base, analyzer);
+
+                if (baseType is null || baseType.ArrayRank == 0)
+                    goto default; // not a array
+
+                if (baseType is ArrayType arrayType)
+                    // flatten the array access
+                    return new ArrayType(arrayType.ElementType, arrayType.ArrayRank - 1);
+                else if (baseType is ScalarType scalarType)
+                    return new ArrayType(scalarType, 0);
+                else
+                    goto default;
+
+            case ReferenceExpressionSyntax referenceExpression
+                when referenceExpression.Identifier.Text is "getint":
+                return new FunctionType(new PrimitiveType(PrimitiveTypeKind.Int));
+
+            case ReferenceExpressionSyntax referenceExpression
+                when referenceExpression.Identifier.Text is "printf":
+                return new FunctionType(new PrimitiveType(PrimitiveTypeKind.Void));
+
+            case ReferenceExpressionSyntax referenceExpression
+                when analyzer.ResolveGlobal(referenceExpression.Identifier.Text) is VariableSymbol variableSymbol:
+                return variableSymbol.Type;
+
+            default:
+                return null;
+        }
+    }
+
+    public static void ConstantModificationAnalysis(ReferenceExpressionSyntax info, ISymbolAnalyzer analyzer, SemanticDiagnosticCollector diagnostics)
+    {
+        if (analyzer.GetClosest<BinaryExpressionSyntax>(b => b.Operator.Token.TokenKind is TokenKind.AssignEqual)
+            is not BinaryExpressionSyntax assignExpression)
+            return;
+
+        if (analyzer.ResolveGlobal(info.Identifier.Text) is not VariableSymbol variableSymbol || variableSymbol.Kind is not SymbolKind.Constant)
+            return;
+
+        bool onLeftSide = false;
+
+        // We must exclude cases where this reference is a part of a function arguments or array indices
+        foreach (var expr in assignExpression.Left.GetChildrenSubtree())
+        {
+            if (expr is FunctionCallExpressionSyntax functionCall && functionCall.ArgumentList.GetChildrenSubtree().Contains(info))
+                return;
+
+            if (expr is ArrayExpressionSyntax arrayAccess && arrayAccess.Index.GetChildrenSubtree().Contains(info))
+                return;
+
+            if (ReferenceEquals(info, expr))
+            {
+                onLeftSide = true;
+                break;
+            }
+        }
+
+        if (onLeftSide)
+        {
+            diagnostics.Add(new SemanticDiagnostic(SemanticErrorKind.AssignmentToConstant, info.Identifier));
+        }
+    }
+
+    public static void RegisterFunctionParameters(BlockSyntax info, ISymbolAnalyzer analyzer, SemanticDiagnosticCollector diagnostics)
+    {
+        var function = analyzer.GetClosest<FunctionDeclarationSyntax>(searchLimit: 2);
+
+        if (function is null)
+            return;
+
+        if (function.Body != info)
+            return;
+
+        if (function.ParameterList is null)
+            return;
+
+        foreach (var parameter in function.ParameterList.Items)
+        {
+            var arrayRank = parameter.ArrayDimensions?.Count ?? 0;
+            var type = ISymbolTypeExtensions.Parse(parameter.Type, arrayRank);
+
+            Debug.Assert(type is not null, "Type should be valid here");
+
+            var symbol = new VariableSymbol(parameter.Identifier, parameter, type, null);
+
+            if (analyzer.CurrentScope?.TryDeclare(symbol) is false)
+            {
+                // probably two parameters with the same name
+                diagnostics.Add(new SemanticDiagnostic(SemanticErrorKind.IdentifierRedefined, symbol.Identifier));
             }
         }
     }
